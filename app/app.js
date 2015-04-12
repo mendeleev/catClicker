3302342d0f984ac28b6878815e95e0cab916979c
Andrii <mendeleev.ua@gmail.com>
2015-04-12 13:14:57 +0300

    v0.0

--- app/app.js
+++ app/app.js
@@ -1,31 +1,8 @@
 (function() {
-    var $container = $('#container'),
-        $template = $('#template').clone(),
-        cats = [
-            {
-                "name": "Tom",
-                "photo": "images/cat1.jpg",
-                "clicks": 0
-            }, 
-            {
-                "name": "Kitti",
-                "photo": "images/cat2.jpg",
-                "clicks": 0
-            }
-        ];
+    var cat = document.getElementById('cat'),
+        counter = document.getElementById('counter');
     
-    $container.empty();    
-    
-    for(var i = 0; i < cats.length; i++) {
-        $template.find('img').attr('src', cats[i].photo);
-        $template.find('.name').text(cats[i].name);
-        $template.find('.counter strong').text(cats[i].clicks);
-        $container.append($template.clone());
-    }
-    
-    $container.on('click', function(event) {
-        var counts = $(event.target).parent().find('.counter strong').text();        
-        $(event.target).parent().find('.counter strong').text(++counts);
+    cat.addEventListener('click', function(event) {
+        counter.innerHTML++;
     });
-    
 })();
